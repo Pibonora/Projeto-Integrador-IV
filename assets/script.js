e704996d847d6ae8072cac565858735ea5fa6dd2
Pietro Bonora <pibonora@outlook.com>
2024-04-18 00:15:43 -0300

    Pequenas correções de Front-End

--- assets/script.js
+++ assets/script.js
@@ -27,3 +27,36 @@ sr.reveal('.home-img', {delay:1000, origin: 'right'});
 
 // ambiente
 
+let perguntasRespostas = [];
+let nome = '';
+
+function adicionarPerguntaResposta() {
+    const nome = prompt("Digite o nome do ambiente:");
+    const pergunta = prompt("Digite a pergunta:");
+    const resposta = prompt("Digite a resposta:");
+    
+    perguntasRespostas.push({ pergunta, resposta });
+    
+    exibirPerguntasRespostas();
+}
+
+function salvar() {
+    nome = document.getElementById('nome').value;
+    
+    // Aqui você poderia enviar os dados para o backend ou armazená-los em um banco de dados.
+    // Por enquanto, vou apenas exibir os dados no console.
+    console.log({ nome, perguntasRespostas });
+}
+
+function exibirPerguntasRespostas() {
+    const container = document.getElementById('container2');
+    container.innerHTML = `<h2>${nome}</h2>`;
+    
+    perguntasRespostas.forEach(item => {
+        i=i+1
+        container.innerHTML += '<div id="wrapper"><div id="pricing-tables"><div class="pricing-table"><div class="header"><div class="title">${nome}</div></div><div class="features"><ul><div class="price">${i} <span>perguntas</span></div></ul></div><div class="link"><a href="#"><span>Estudar</span></a></div></div>'
+        container.innerHTML += `<p><strong>${i}</strong> ${item.resposta}</p>`;
+        container.innerHTML += `<p><strong>Pergunta:</strong> ${item.pergunta}</p>`;
+        container.innerHTML += `<p><strong>Resposta:</strong> ${item.resposta}</p>`;
+    });
+}
