6d1c1ebc3c0fbf983c86baa8267d5837de30b1f5
PIETRO  BONORA <0220832221023@fatectq.local>
2024-04-19 11:05:18 -0300

    Cards

--- assets/script.js
+++ assets/script.js
@@ -21,9 +21,12 @@ sr.reveal('.menu-btn', {delay:520, origin: 'right'});
 sr.reveal('.home-text span', {delay:600, origin: 'top'});
 sr.reveal('.home-text h1', {delay:680, origin: 'left'});
 sr.reveal('.home-text p', {delay:750, origin: 'right'});
-sr.reveal('.main-btn', {delay:860, origin: 'left'});
-sr.reveal('.share', {delay:950, origin: 'bottom'});
+sr.reveal('.main-btn', {delay:860, origin: 'bottom'});
+sr.reveal('.social', {delay:950, origin: 'bottom'});
 sr.reveal('.home-img', {delay:1000, origin: 'right'});
+sr.reveal('.container-card', {delay:500, origin: 'right'});
+sr.reveal('.menu-btn-constraste', {delay:600, origin: 'right'});
+sr.reveal('.VLibras', {delay:700, origin: 'right'});
 
 // ambiente
 
