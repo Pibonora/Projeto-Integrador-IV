deba4e2847e1417138680c2955deffc72ea7b6eb
Pibonora <112083122+Pibonora@users.noreply.github.com>
2024-06-19 17:13:25 -0300

    Update firebaseConfig.js

--- assets/firebaseConfig.js
+++ assets/firebaseConfig.js
@@ -1,11 +1,5 @@
 // firebaseConfig.js
 const firebaseConfig = {
-    apiKey: "AIzaSyBLonRu4hj6dBIJqgJdc5G-udCcVpHF3qU",
-    authDomain: "pi04-d0ced.firebaseapp.com",
-    databaseURL: "https://pi04-d0ced-default-rtdb.firebaseio.com",
-    projectId: "pi04-d0ced",
-    storageBucket: "pi04-d0ced.appspot.com",
-    messagingSenderId: "308368487306",
-    appId: "1:308368487306:web:3240947efc7d0e7a984e73"
+    
   };
 export default firebaseConfig;
